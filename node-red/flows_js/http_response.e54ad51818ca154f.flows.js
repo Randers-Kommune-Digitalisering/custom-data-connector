ce619214afe552d1b9181c162ee174f13971ee71
rak-randers <rune.aagaard.keena@randers.dk>
2023-12-04 08:57:38 +0100

    -

---
+++ node-red/flows_js/http_response.e54ad51818ca154f.flows.js
@@ -0,0 +1,15 @@
+const Node = {
+  "id": "e54ad51818ca154f",
+  "type": "http response",
+  "z": "971a7ae6df987a48",
+  "g": "0dec742373825c9e",
+  "name": "",
+  "statusCode": "",
+  "headers": {},
+  "x": 470,
+  "y": 60,
+  "wires": [],
+  "_order": 198
+}
+
+module.exports = Node;
